98d50ad6ef6f0bcd3e2eb6626671dbc34eb770b9
ktij <38995851+ktij@users.noreply.github.com>
2022-05-26 17:00:04 +1000

    Added more categories

--- controllers/extractCategoryF.js
+++ controllers/extractCategoryF.js
@@ -16,7 +16,7 @@ async function extractCategoryF(fileName) {
             } else {
                 var text = results[0].textAnnotations[0].description;
                 text = text.toUpperCase();
-                var categories = [['BISCUIT','COOKIE'], ['BAR'], ['CHEESE','CHEDDAR']];
+                var categories = [['BISCUIT','COOKIE'], ['BAR'], ['CHEESE','CHEDDAR'], ['WRAP'], ['MILK']];
 
                 var cat = '';
                 
